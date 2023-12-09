212359d3fb9d32e32c90f56e45080d7a167cb2d8
Pavlo0164 <126184203+Pavlo0164@users.noreply.github.com>
2023-12-09 22:15:54 +0200

    Delete lesson_18 directory

--- lesson_18/js/app-two.js
+++
@@ -1,5 +0,0 @@
-const headerBurger = document.querySelector('.header__burger')
-const menu = document.querySelector('.menu')
-headerBurger.addEventListener('click',function(){
-    menu.classList.toggle("open-menu")
-})
--- lesson_18/js/app.js
+++
@@ -1,5 +0,0 @@
-const headerBurger = document.querySelector('.header__burger');
-const header = document.querySelector('.header');
-headerBurger.addEventListener("click", function(){
-    header.classList.toggle("active")
-});
